cead1ac5e4def9d965e09d03769d5a9c434eb2d9
Anton Sineev <sineevanton@mail.ru>
2022-06-02 20:54:18 +0300

    Task52 complete

---
+++ Task52/Task52.cs
@@ -0,0 +1,35 @@
+﻿// Specify a two-dimensional array of integers.
+// Find the arithmetic mean of the elements in each
+// column.
+
+int rows = 3;
+int cols = 4;
+
+int[,] CreateAndShowArrayRowXCol(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random rnd = new Random();
+    for (int r = 0; r < row; r++)
+    {
+        for (int c = 0; c < col; c++)
+        {
+            array[r, c] = rnd.Next(0, 6);
+            Console.Write(array[r, c] + "\t");
+        }
+    Console.WriteLine();
+    }
+    return array;
+}
+
+int[,] arr = CreateAndShowArrayRowXCol(rows, cols);
+
+Console.Write("Arithmetic mean of the elements in each column: ");
+for (int i = 0; i < cols; i++)
+{
+    double summa = 0.0;
+    for (int j = 0; j < rows; j++)
+    {
+        summa += arr[j, i];
+    }
+    Console.Write(Math.Round(summa / rows, 2) + ", ");
+}
